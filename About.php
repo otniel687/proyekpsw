eb42e68f5b70902e40c00ce893bbac75d8eb4dbb
IndahCSS <82864019+IndahCSS@users.noreply.github.com>
2021-04-22 11:00:28 +0700

    Update About.php

--- About.php
+++ About.php
@@ -13,36 +13,36 @@
 <body background="css/Image/background.jpg" style="background-repeat: no-repeat; background-size: 100%">
   <!--Navbar-->
   <header>
-    <nav class="navbar navbar-expand-lg navbar-inverse navbar-light">
-      <div class="container">
-        <a class="navbar-brand" href="#">
-          <h1>AOTLIE Service</h1>
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+      <nav class="navbar navbar-expand-lg navbar-inverse navbar-light">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <h1>AOTLIE Service</h1>
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="About.html">About</a>
+                <a class="nav-link" aria-current="page" href="About.php">About</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" href="Service.html">Service</a>
+                <a class="nav-link active" href="Service.php">Service</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="Registration.html">Registration</a>
+                <a class="nav-link" href="Registration.php">Registration</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="Contact Us.html">Contact Us</a>
+                <a class="nav-link" href="Contact Us.php">Contact Us</a>
               </li>
               <li class="nav-item">
-                <a class="nav-item btn btn-primary tombol" href="login.html">Login</a>
+                <a class="nav-item btn btn-primary tombol" href="login.php">Login</a>
               </li>
             </ul>
           </div>
-      </div>
-    </nav>
-  </header>
+        </div>
+      </nav>
+    </header>
 
   <div class="container">
     <H2><br>Informasi Singkat</H2><br>
